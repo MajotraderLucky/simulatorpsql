3eba619db894fed91abd646261f66ceca43c0fd2
ryazanovs <sergey.ryazanov@sfera.zone>
2024-05-20 03:19:01 +0300

    feat(main): Add PostgreSQL database ping functionality in Go
    - Implemented a function `pingDatabase` to establish a connection and ping a PostgreSQL database using the `lib/pq` driver.
    - Configured database connection parameters (host, port, user, password, dbname) based on the settings from the docker-compose.yml.
    - Added error handling to gracefully manage failed connections and pings.
    - Updated the `main` function to call `pingDatabase` and log any errors if the connection or ping fails.
    
    This ensures that the application can verify connectivity with the PostgreSQL database set up via Docker Compose before proceeding with further operations.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"        // Или используйте имя хоста Docker, если запускаете из другого контейнера
+	port     = 5432               // Порт, на котором работает PostgreSQL
+	user     = "your_db_user"     // Имя пользователя, указанное в docker-compose.yml
+	password = "your_db_password" // Пароль пользователя, указанный в docker-compose.yml
+	dbname   = "your_db_name"     // Имя базы данных, указанное в docker-compose.yml
+)
+
+func pingDatabase() error {
+	// Формируем строку подключения
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	// Пытаемся подключиться к базе данных
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return fmt.Errorf("error opening database: %v", err)
+	}
+	defer db.Close()
+
+	// Пингуем базу данных
+	err = db.Ping()
+	if err != nil {
+		return fmt.Errorf("error pinging database: %v", err)
+	}
+
+	fmt.Println("Successfully connected and pinged PostgreSQL database!")
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello, Simulator Pg-GRPC!")
+	err := pingDatabase()
+	if err != nil {
+		log.Fatalf("Failed to ping database: %v", err)
+	}
 }
